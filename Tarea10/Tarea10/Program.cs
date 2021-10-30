29b68a0699ea1d1ffe617e6060be1e68b5b34450
ignacio Vela <89303905+est-dato@users.noreply.github.com>
2021-10-29 22:21:04 -0300

    Add files via upload

---
+++ Tarea10/Tarea10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            string linea;
+            Console.Write("Ingrese un numero: ");
+            linea = Console.ReadLine();
+            num1 = int.Parse(linea);
+            if (num1 % 2 == 0)
+            {
+            Console.Write("Este numero es par");
+            } 
+            else
+            {
+            Console.Write("El numero es impar");
+            }
+            Console.ReadKey();
+        }
+    }
+}
